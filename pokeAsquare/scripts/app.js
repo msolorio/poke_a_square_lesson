9b6e0179657f51b6a874235235970c919ddf7d1b
daltonhart <daltonvaren@gmail.com>
2019-09-30 09:55:25 -0700

    modifies user stories

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,8 +1,8 @@
 console.log('Welcome to Poke-A-Square...');
 
 // == User Stories/game logic == //
-// When the user clicks begin the timer should start, and the squares should populate with a random color
+// When the user clicks begin, the timer should start and the squares should populate with a random color
 
-// When the use clicks on a color the color should disapear, and score should be added or subtracted
+// When the user clicks on a color: the color should disapear and score should be added or subtracted.
 
-// When the round is over the scores round and time should be updated and the user should be able to start again with increased difficulty.
+// When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
