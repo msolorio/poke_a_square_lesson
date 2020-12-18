47a07238ef9f15b5c1dc157c932c453fc1d1ff98
msolorio-ga <michael.solorio@generalassemb.ly>
2020-12-18 12:56:56 -0800

    Update app.js

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,8 +1,13 @@
 console.log('Welcome to Poke-A-Square...');
 
-// == User Stories/game logic == //
-// When the user clicks begin, the timer should start and the squares should populate with a random color
+/*
+SPECS
 
-// When the user clicks on a color: the color should disapear and score should be added or subtracted.
+When the user clicks begin the timer should start counting down, and the squares should populate with a random color, either red, green, or blue.
 
-// When the round is over, the scores round and timer should be updated for the user to start over with increased difficulty.
+When the user clicks on a color the color should become invisible. If the square is blue add one to the score. If the square is red or green the subtract one from the score.
+
+If the user clicks all the blue squares, take them to the next round, repopulate the board with more squares, and restart the timer.
+
+If the timer gets to 0 update the display to show Game Over and show the score.
+*/
